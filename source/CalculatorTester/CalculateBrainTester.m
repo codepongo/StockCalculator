c92e267db943cdd380d6b48d39a9bf95fe5256d1
zuohaitao <zhuhuotui@gmail.com>
2016-09-27 10:05:52 +0800

    tester loads data

--- source/CalculatorTester/CalculateBrainTester.m
+++ source/CalculatorTester/CalculateBrainTester.m
@@ -11,8 +11,25 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        CalculateBrain* brain = [[CalculateBrain alloc]init];
+        NSError* err;
+        NSString* folder = [@"~/Documents/trade" stringByExpandingTildeInPath];
+        if (argv[1] != NULL) {
+            folder = [[NSString alloc] initWithUTF8String:argv[1]];
+        }
+        NSArray* files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:folder error:&err];
+        //NSSearchPathForDirectoriesInDomains(
+        for (NSString* file in files) {
+            if ([[file pathExtension] isEqualToString:@"txt"]) {
+                //NSStringEncoding encode = NSUTF8StringEncoding;
+                NSStringEncoding encode = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingDOSChineseSimplif);
+                NSData* data = [NSData dataWithContentsOfFile:[folder stringByAppendingPathComponent:file]];
+                NSString* s = [[NSString alloc] initWithData:[data subdataWithRange:NSMakeRange(0, [data length])] encoding:encode];
+                NSLog(@"%@", s);
+            }
+            
+        }
+
+        //CalculateBrain* brain = [[CalculateBrain alloc]init];
     }
     return 0;
 }
