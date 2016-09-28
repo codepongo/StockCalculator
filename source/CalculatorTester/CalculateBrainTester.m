5b5fe1e49f11d4423a9de78003a695dc445b71b3
zuohaitao <zhuhuotui@gmail.com>
2016-09-28 09:47:19 +0800

    parse file

--- source/CalculatorTester/CalculateBrainTester.m
+++ source/CalculatorTester/CalculateBrainTester.m
@@ -17,18 +17,44 @@ int main(int argc, const char * argv[]) {
             folder = [[NSString alloc] initWithUTF8String:argv[1]];
         }
         NSArray* files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:folder error:&err];
-        //NSSearchPathForDirectoriesInDomains(
+        NSMutableArray* record = [NSMutableArray arrayWithCapacity:[files count] * 10];
+        
         for (NSString* file in files) {
-            if ([[file pathExtension] isEqualToString:@"txt"]) {
-                //NSStringEncoding encode = NSUTF8StringEncoding;
-                NSStringEncoding encode = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingDOSChineseSimplif);
-                NSData* data = [NSData dataWithContentsOfFile:[folder stringByAppendingPathComponent:file]];
-                NSString* s = [[NSString alloc] initWithData:[data subdataWithRange:NSMakeRange(0, [data length])] encoding:encode];
-                NSLog(@"%@", s);
+            if (![[file pathExtension] isEqualToString:@"txt"]) {
+                continue;
+            }
+            NSDictionary* template = @{
+                @"证券名称":@0,
+                @"成交日期":@1,
+                @"成交价格":@2,
+                @"成交数量":@5,
+                @"发生金额":@8,
+                @"业务名称":@19,
+                @"手续费":@20,
+                @"印花税":@23,
+                @"过户费":@26,
+                @"结算费":@29,
+                };
+            NSStringEncoding encode = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingDOSChineseSimplif);
+            NSData* data = [NSData dataWithContentsOfFile:[folder stringByAppendingPathComponent:file]];
+            NSString* s = [[NSString alloc] initWithData:[data subdataWithRange:NSMakeRange(0, [data length])] encoding:encode];
+            NSArray* a = [s componentsSeparatedByString:@"\n"];
+            NSLog(@"%@", file);
+            for (NSString* l in [a subarrayWithRange:NSMakeRange(1, [a count] - 1)]) {
+                if ([l length] == 0) {
+                    continue;
+                }
+                //NSLog(l);
+                NSArray* field = [l componentsSeparatedByString:@" "];
+                NSMutableDictionary *r = [NSMutableDictionary dictionaryWithDictionary:template];
+                for (NSString* k in template) {
+                    r[k] = [field objectAtIndex:[r[k] integerValue]];
+                }
+                [record addObject:r];
+                
             }
             
         }
-
         //CalculateBrain* brain = [[CalculateBrain alloc]init];
     }
     return 0;
